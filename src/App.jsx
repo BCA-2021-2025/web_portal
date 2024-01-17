7e60a01baea03dbf74f9aaead11ed11bd7ec8617
nilaw2017 <nilaw.manandhar2016@gmail.com>
2024-01-17 20:59:46 +0545

    Initializing web portal

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Page404 from "./pages/Page404";
+import Index from "./pages/homepage/views";
+import AboutUs from "./pages/homepage/views/AboutUs";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Page404 />} />
+        <Route path="/" element={<Index />}>
+          <Route path="aboutus" element={<AboutUs/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
